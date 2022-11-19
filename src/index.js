90d577ed4efd8b50666770c51b09dcbb06517aed
Ernesto García <ernesto_garciat@outlook.com>
2022-11-19 17:28:21 -0600

    Add prompt to read files on console

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { Parser } = require("jison");
+const prompt = require('prompt');
 const path = require("path");
 const { readFileSync } = require("fs");
 const Quadruple = require('./quadruple.js');
@@ -10,6 +11,19 @@ const quadruple = new Quadruple();
 parser.yy.data = {
     quadruple
 };
-parser.parse(readFileSync('./test_6.txt', "utf-8"));
-const vm = new VM(quadruple);
-vm.run();
+
+prompt.start();
+
+prompt.get(['filename'], function (err, result) {
+    if (err) {
+        return onErr(err);
+    }
+    parser.parse(readFileSync(result.filename, "utf-8"));
+    const vm = new VM(quadruple);
+    vm.run();
+});
+
+function onErr(err) {
+    console.log(err);
+    return 1;
+}
